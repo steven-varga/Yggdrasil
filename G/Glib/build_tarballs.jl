e711b1f418b468ce64642fd2a4f81793547dd371
Mosè Giordano <m.giordano@ucl.ac.uk>
2019-09-20 22:16:12 +0100

    [Glib] Upgrade builder to v2.62

--- G/Glib/build_tarballs.jl
+++ G/Glib/build_tarballs.jl
@@ -1,20 +1,39 @@
 using BinaryBuilder
 
 name = "Glib"
-version = v"2.59.0"
+version = v"2.62.0"
 
 # Collection of sources required to build Glib
 sources = [
     "https://ftp.gnome.org/pub/gnome/sources/glib/$(version.major).$(version.minor)/glib-$(version).tar.xz" =>
-    "664a5dee7307384bb074955f8e5891c7cecece349bbcc8a8311890dc185b428e",
+    "6c257205a0a343b662c9961a58bb4ba1f1e31c82f5c6b909ec741194abc3da10",
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/glib-*/
 mkdir build_glib && cd build_glib
-meson .. -Dman=false --cross-file="${MESON_TARGET_TOOLCHAIN}"
 
+SED_SCRIPT=()
+if [[ "${target}" == i686-linux-musl ]]; then
+    # We can't run executables for i686-linux-musl in the BB environment
+    SED_SCRIPT+=(-e "s/needs_exe_wrapper = false/needs_exe_wrapper = true/" "${MESON_TARGET_TOOLCHAIN}")
+elif [[ "${target}" == *-mingw* ]]; then
+    # Tell meson where to find libintl.h
+    SED_SCRIPT+=(-e "s?c_args = \[]?c_args = ['-I${prefix}/include']?")
+fi
+
+# We need to link to iconv, but ninja doesn't know how to do that as libiconv
+# doesn't have a pkgconfig file.  Let's give meson a tip
+sed -i -e "s/c_link_args = \[]/c_link_args = ['-liconv']/" \
+    "${SED_SCRIPT[@]}" \
+    "${MESON_TARGET_TOOLCHAIN}"
+# We need a new version of objcopy (in binutils) and a native "msgfmt" (in gettext)
+apk add binutils gettext
+# Get rid of the old objcopy
+rm /opt/bin/objcopy
+
+meson .. -Dman=false --cross-file="${MESON_TARGET_TOOLCHAIN}"
 ninja -j${nproc}
 ninja install
 """
@@ -25,11 +44,11 @@ platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = [
-    LibraryProduct("libgio", :libgio),
-    LibraryProduct("libglib", :libglib),
-    LibraryProduct("libgmodule", :libgmodule),
-    LibraryProduct("libgobject", :libgobject),
-    LibraryProduct("libgthread", :libgthread),
+    LibraryProduct(["libgio-2", "libgio-2.0"], :libgio),
+    LibraryProduct(["libglib-2", "libglib-2.0"], :libglib),
+    LibraryProduct(["libgmodule-2", "libgmodule-2.0"], :libgmodule),
+    LibraryProduct(["libgobject-2", "libgobject-2.0"], :libgobject),
+    LibraryProduct(["libgthread-2", "libgthread-2.0"], :libgthread),
 ]
 
 # Dependencies that must be installed before this package can be built
@@ -42,4 +61,4 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"8")
