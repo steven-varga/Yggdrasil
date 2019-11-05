240c004b3680693a26dfea5b75b1ca78e412eefc
Mosè Giordano <m.giordano@ucl.ac.uk>
2019-11-05 22:59:29 +0000

    Build Bzip2 and Zlib with Docker

--- B/Bzip2/build_tarballs.jl
+++ B/Bzip2/build_tarballs.jl
@@ -74,3 +74,4 @@ dependencies = [
 ]
 
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+
--- Z/Zlib/build_tarballs.jl
+++ Z/Zlib/build_tarballs.jl
@@ -33,3 +33,4 @@ dependencies = [
 ]
 
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+
